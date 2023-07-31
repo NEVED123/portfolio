a100afb70f9ad3c66b825bcb3a7301a0315f7cba
NEVED123 <neved1234@gmail.com>
2023-07-30 20:46:21 -0400

    big refactor, work stuff works

--- js/accordionManager.js
+++ js/accordionManager.js
@@ -1,53 +1,35 @@
-current = undefined
-
 function openElement(clickedDiv){
 
-    if(current == clickedDiv.id) return
+    const current = clickedDiv.id
 
-    if(current == undefined){
-        current = clickedDiv.id
+    //get group
+    const groupParent = clickedDiv.parentElement.parentElement
 
-        const siblings = Array.from(clickedDiv.parentElement.children)
+    //iterate through all elements, hide all but our current one
+    //if we are on our current one, remove the hidden attribute
 
-        //console.log(siblings)
-    
-        siblings.forEach((sibling)=>{
-            if(sibling.className != clickedDiv.className){
-                //show
-                sibling.classList.remove('hidden')
+    const groupElements = Array.from(groupParent.children)
+
+    //console.log(groupElements)
+
+    groupElements.forEach((element)=>{
+
+        const children = Array.from(element.children)
+
+        children.forEach((child)=>{
+            
+            if(!child.classList.contains('visible')){
+                if(child.id == current){
+                    child.classList.remove('hidden')
+                }
+                else{
+                    if(!child.classList.contains('hidden'))
+                    child.classList.add('hidden')
+                }
             }
         })
 
-        return
-    }
-
-    //get the siblings for the one we need to close
-
-    const siblings = Array.from(clickedDiv.parentElement.children)
-
-    //console.log(siblings)
-
-    siblings.forEach((sibling)=>{
-        if(sibling.className != clickedDiv.className){
-            //show
-            sibling.classList.remove('hidden')
-        }
     })
-    
-    //get id of current
-    
-    container = clickedDiv.parentElement.parentElement
-
-    toHide = container.querySelectorAll('#' + CSS.escape(current))
-
-    toHide.forEach((sibling)=>{
-        if(sibling.className != clickedDiv.className){
-            //show
-            sibling.classList.add('hidden')
-        }
-    })
-
-    current = clickedDiv.id
 
 }
 
--- js/carouselGenerator.js
+++ js/carouselGenerator.js
@@ -1,12 +1,18 @@
 window.addEventListener("load", ()=>{
     fetch('../info/projects.json')
     .then((response) => response.json())
-    .then((json) => parseProjects(json));
+    .then((json) => parseProjects(json))
+
+    fetch('../info/work.json')
+    .then((response) => response.json())
+    .then((json) => parseWork(json))
 })
 
-let carouselNumber = 0;
+let carouselNumber = 0
 
 async function parseProjects(projects){
+
+    carouselNumber = 0
 
     const project = document.createDocumentFragment();
 
@@ -16,18 +22,20 @@ async function parseProjects(projects){
 
         //create div to wrap each carousel
         const carousel = document.createElement('div')
-        carousel.setAttribute('class','projects-contents__project')
+        carousel.setAttribute('class','info-contents__info')
+        carousel.setAttribute('id',carouselNumber.toString())
 
         //create title for project
         const projectTitle = document.createElement('div')
-        projectTitle.setAttribute('class','projects-contents__project__title')
+        projectTitle.setAttribute('class','infos-contents__info__title')
+        projectTitle.classList.add('class','visible')
         projectTitle.setAttribute('id',carouselNumber.toString())
         projectTitle.setAttribute('onClick','openElement(this)')
         projectTitle.appendChild(document.createTextNode(category.name))
 
         //create description for project
         const projectDesc = document.createElement('div')
-        projectDesc.setAttribute('class','project-contents__project__description hidden')
+        projectDesc.setAttribute('class','info-contents__info__description hidden')
         projectDesc.setAttribute('id',carouselNumber.toString())
         projectDesc.appendChild(document.createTextNode(category.description))
 
@@ -35,43 +43,9 @@ async function parseProjects(projects){
         carousel.appendChild(projectDesc)
 
         if(category.assets != null){
-
-            //carousel frame
-            const carouselFrame = document.createElement('div')
-            carouselFrame.setAttribute('class', 'carousel__frame hidden')
-            carouselFrame.setAttribute('id',carouselNumber.toString())
-
-            const leftArrow = document.createElement('a')
-            leftArrow.setAttribute('class', 'carousel__frame__arrow--left')
-            leftArrow.setAttribute('onClick', 'moveLeft(this)')
-            leftArrow.appendChild(document.createTextNode('<'))
-
-            const rightArrow = document.createElement('a')
-            rightArrow.setAttribute('class', 'carousel__frame__arrow--right')
-            rightArrow.setAttribute('onClick', 'moveRight(this)')
-            rightArrow.appendChild(document.createTextNode('>'))
-
-            carouselFrame.appendChild(leftArrow)
-            carouselFrame.appendChild(rightArrow)
-
             const assetsPath = `../assets/projects/${category.assets}`
-
-            await getFilePathsFromDirectory(assetsPath).then((paths)=>{
-                
-                paths.forEach((path)=>{
-                    const image = generateCarouselImage(path)
-                    carouselFrame.appendChild(image)
-                })
-    
-                if(paths.length > 0){
-                    const firstImage = generateCarouselImage(paths[0])
-                    carouselFrame.appendChild(firstImage)
-                }
-            })
-
-            carousel.appendChild(carouselFrame)
-            
-            
+            const carouselFrame = await generateCarousel(assetsPath, carouselNumber)
+            carousel.appendChild(carouselFrame)    
         }
         else{
             projectDesc.setAttribute('style', 'padding: 20px 0px 20px 0px')
@@ -80,6 +54,113 @@ async function parseProjects(projects){
         project.appendChild(carousel)
         document.getElementById('project-set').appendChild(project)
     })
+}
+
+async function parseWork(work){
+
+    carouselNumber = 0
+
+    const project = document.createDocumentFragment();
+
+    await work.categories.forEach(async (category)=>{
+
+        carouselNumber++;
+
+        //create div to wrap each carousel
+        const carousel = document.createElement('div')
+        carousel.setAttribute('class','info-contents__info')
+        carousel.setAttribute('id',carouselNumber.toString())
+
+        //create title for project
+        const workTitle = document.createElement('div')
+        workTitle.setAttribute('class','infos-contents__info__title visible')
+        workTitle.setAttribute('id',carouselNumber.toString())
+        workTitle.setAttribute('onClick','openElement(this)')
+        workTitle.appendChild(document.createTextNode(category.name))
+        
+
+        const position = document.createElement('div')
+        position.setAttribute('class','infos-contents__info__subtitle hidden')
+        position.setAttribute('id',carouselNumber.toString())
+        position.appendChild(document.createTextNode(category.position))
+
+        const dates = document.createElement('div')
+        dates.setAttribute('class','infos-contents__info__subtitle hidden')
+        dates.setAttribute('id',carouselNumber.toString())
+        dates.appendChild(document.createTextNode(category.dates))
+
+
+        //create description for project
+        const projectDesc = document.createElement('div')
+        projectDesc.setAttribute('class','info-contents__info__description hidden')
+        projectDesc.setAttribute('id',carouselNumber.toString())
+        projectDesc.appendChild(document.createTextNode(category.description))
+
+        carousel.appendChild(workTitle)
+        carousel.appendChild(dates)
+        carousel.appendChild(position)
+        carousel.appendChild(projectDesc)
+
+        if(category.assets != null){
+            const assetsPath = `../assets/work/${category.assets}`
+            const carouselFrame = await generateCarousel(assetsPath, carouselNumber)
+            carousel.appendChild(carouselFrame)    
+        }
+        else{
+            projectDesc.setAttribute('style', 'padding: 20px 0px 20px 0px')
+        }
+
+        project.appendChild(carousel)
+        document.getElementById('work-set').appendChild(project)
+    })
+}
+
+async function generateCarousel(assetsPath, id){
+    //carousel frame
+    const carouselFrame = document.createElement('div')
+    carouselFrame.setAttribute('class', 'carousel__frame hidden')
+    carouselFrame.setAttribute('id',id.toString())
+
+    const leftArrow = document.createElement('a')
+    leftArrow.setAttribute('class', 'carousel__frame__arrow--left')
+    leftArrow.setAttribute('onClick', 'moveLeft(this)')
+    leftArrow.appendChild(document.createTextNode('<'))
+
+    const rightArrow = document.createElement('a')
+    rightArrow.setAttribute('class', 'carousel__frame__arrow--right')
+    rightArrow.setAttribute('onClick', 'moveRight(this)')
+    rightArrow.appendChild(document.createTextNode('>'))
+
+    carouselFrame.appendChild(leftArrow)
+    carouselFrame.appendChild(rightArrow)
+
+    await getFilePathsFromDirectory(assetsPath).then((paths)=>{
+        
+        paths.forEach((path)=>{
+            const image = generateCarouselImage(path)
+            carouselFrame.appendChild(image)
+        })
+
+        if(paths.length > 0){
+            const firstImage = generateCarouselImage(paths[0])
+            carouselFrame.appendChild(firstImage)
+        }
+    })
+
+    return carouselFrame
+}
+
+function generateCarouselImage(src){
+    const imageWrapper = document.createElement('div')
+    imageWrapper.setAttribute('class', 'carousel__image-frame')
+
+    const image = document.createElement('img')
+    image.setAttribute('class', 'carousel__image')
+    image.setAttribute('src', src)
+
+    imageWrapper.appendChild(image)
+
+    return imageWrapper;
 }
 
 function getFilePathsFromDirectory(filepath) {
@@ -106,15 +187,5 @@ function getFilePathsFromDirectory(filepath) {
 
 }
 
-function generateCarouselImage(src){
-    const imageWrapper = document.createElement('div')
-    imageWrapper.setAttribute('class', 'carousel__image-frame')
 
-    const image = document.createElement('img')
-    image.setAttribute('class', 'carousel__image')
-    image.setAttribute('src', src)
 
-    imageWrapper.appendChild(image)
-
-    return imageWrapper;
-}
