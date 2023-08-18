6416a2ec92268019c1500472fa1e9cde112b5ace
NEVED123 <neved1234@gmail.com>
2023-08-18 13:42:31 -0400

    keyboard is janky but functional

--- js/fun.js
+++ js/fun.js
@@ -1,6 +1,31 @@
-document.addEventListener("DOMContentLoaded", ()=> initialize())
+document.addEventListener("DOMContentLoaded", ()=> {
+    initializePhysics()
+    initializePiano()
+})
 
-function initialize() {
+let notes = [
+    "C4", 
+    "Csharp4", 
+    "D4", 
+    "Dsharp4", 
+    "E4", 
+    "F4",
+    "Fsharp4",
+    "G4",
+    "Gsharp4",
+    "A4",
+    "Asharp4",
+    "B4",
+    "C5",
+    "Csharp5",
+    "D5",
+    "Dsharp5",
+    "E5",
+    "F5",
+    "Fsharp5",
+    "G5"];
+
+function initializePhysics() {
     console.log("Initializing Physics Engine")
     
     var Engine = Matter.Engine,
@@ -12,7 +37,7 @@ function initialize() {
 // create an engine
 var engine = Engine.create();
 
-var funPage = document.getElementById("fun");
+var funPage = document.getElementsByClassName("physics")[0];
 
 // create a renderer
 var render = Render.create({
@@ -37,3 +62,55 @@ var runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
 }
+
+function initializePiano() {
+
+    for(let i = 0;i<notes.length; i++){
+        const noteName = notes[i];
+        const audio = new Audio(`../assets/sounds/${noteName}.mp3`)
+
+        const noteElement = document.getElementById(noteName)
+
+        const startEvents = ["mousedown", "mouseenter"]
+
+        startEvents.forEach(function(e){
+            noteElement.addEventListener(e, (event)=>{
+
+                if(noteName.includes("sharp")){
+                    event.stopPropagation()
+                }
+                
+                if(e == "mouseenter" && event.buttons == 1 || e == "mousedown"){
+                    audio.play()
+                }  
+            })
+        })
+
+        const cancelEvents = ["mouseup", "mouseout"]
+
+        cancelEvents.forEach(function(e){
+            noteElement.addEventListener(e, ()=>{
+                audio.pause()
+                audio.load()
+            })
+        })  
+
+    }
+}
+
+// function fadeAudio(audio){
+
+//     let miliseconds = 0;
+//     const interval = 10;
+
+//     let fadeInterval = setInterval(()=>{
+//         audio.volume = parseFloat(audio.volume - .2).toPrecision(12)
+//         miliseconds += interval
+//         if(Math.floor(miliseconds) == 50){
+//             audio.pause()
+//             audio.volume = 1;
+//             audio.load()
+//             clearInterval(fadeInterval)
+//         }
+//     },interval)
+// }
