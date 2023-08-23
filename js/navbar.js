c612a99c1fdb527426b3d6bc5022ac648fb3dd3e
NEVED123 <neved1234@gmail.com>
2023-08-23 17:16:24 -0400

    navbar items now glow when scrolling to section

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,39 @@
+let pages
+
+addEventListener("scroll", ()=>{
+  //idea: check if a div is more than 50% on the screen
+  //coordinates of div: between 0 and divheight/2
+
+  pages = [
+    document.getElementById("skills"), 
+    document.getElementById("projects"),
+    document.getElementById("work"),
+    document.getElementById("aboutme"),
+    document.getElementById("resume"),
+    document.getElementById("fun")
+  ]
+
+  pages.forEach((page)=>{
+
+    const navbarItem = document.querySelector(`a.navbar__item[href="#${page.id}"]`)
+    if(isHalfwayInViewPort(page)){  
+      if(!navbarItem.classList.contains("glowing")){
+        navbarItem.classList.add("glowing")
+      }
+    }
+    else
+    {
+      navbarItem.classList.remove("glowing")
+    }
+  })
+})
+
+function isHalfwayInViewPort(element){
+  const rect = element.getBoundingClientRect();
+
+  if(rect.top > -rect.height / 2 && rect.top < rect.height / 2) return true;
+}
+
 function toggleNavbar(){
     var x = document.getElementById("navbar")
 
@@ -13,3 +49,4 @@ function toggleNavbar(){
 
     }
 }
+
